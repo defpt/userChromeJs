1fd190efb82d643aa8f41c16e30ba0a8411fb1f2
defpt <defpt@outlook.com>
2013-07-16 08:46:34 +0200

    添加某项

--- TabPlus.uc.js
+++ TabPlus.uc.js
@@ -1,38 +1,47 @@
+(function() {
 // ==UserScript==
-// @name			TabPlus.uc.js
-// @description		修改整合版标签增强
+// @name				NewTabPlus_mod
+// @description	    整合版标签增强
 // @include			chrome://browser/content/browser.xul
 // @include			chrome://browser/content/bookmarks/bookmarksPanel.xul
 // @include			chrome://browser/content/history/history-panel.xul
 // @include			chrome://browser/content/places/places.xul
-// @compatibility	Firefox 18
 // ==/UserScript==
-(function() {
-    //总在当前标签页打开Bookmarklet
-    eval("openLinkIn = " + openLinkIn.toString().replace(/(?=if \(where == "save"\))/, 'if (url.substr(0, 11) == "javascript:") where = "current";').replace(/(?=var loadInBackground)/, 'if (w.gBrowser.currentURI.spec == "about:blank" && !w.gBrowser.mCurrentTab.hasAttribute("busy")) where = "current";'));
-    //新标签打开:书签、历史、搜索栏
+
+	// 新标签打开:书签、历史、搜索栏
+	try {
+		eval('openLinkIn=' + openLinkIn.toString().
+		replace('w.gBrowser.selectedTab.pinned', '(!w.isTabEmpty(w.gBrowser.selectedTab) || $&)').
+		replace(/&&\s+w\.gBrowser\.currentURI\.host != uriObj\.host/, ''));
+    }catch(e){}
+
+    // 地址栏新标签打开
+   try {
+		location=="chrome://browser/content/browser.xul" && 
+		eval("gURLBar.handleCommand="+gURLBar.handleCommand.toString().replace(/^\s*(load.+);/gm,
+		"if(/^javascript:/.test(url)||isTabEmpty(gBrowser.selectedTab)){loadCurrent();}else{this.handleRevert();gBrowser.loadOneTab(url, {postData: postData, inBackground: false, allowThirdPartyFixup: true});}"));
+    }catch(e){}
+
+	/*主页新标签打开*/
     try {
-        str = openLinkIn.toString();
-        str = str.replace('w.gBrowser.selectedTab.pinned', '(!w.isTabEmpty(w.gBrowser.selectedTab) || $&)');
-        str = str.replace(/ &&\s+w\.gBrowser\.currentURI\.host != uriObj\.host/, '');
-        str = str.replace(/loadInBackground = false;/g, 'loadInBackground = aInBackground;');
-        eval('openLinkIn = ' + str);
-    } catch(e) {}
-    //地址栏新标签打开
-    try {
-        if (location.href == 'chrome://browser/content/browser.xul') {
-            str = gURLBar.handleCommand.toString();
-            str = str.replace('&& !isTabEmpty', '|| isTabEmpty');
-            str = str.replace('|| altEnter', '|| !altEnter');
-            eval('gURLBar.handleCommand = ' + str);
-        }
-    } catch(e) {}
-    //中键点击bookmark菜单不关闭
+        eval("BrowserGoHome = " + BrowserGoHome.toString().replace(
+            /switch \(where\) {/, "where = (gBrowser.currentURI.spec!="
+            +"'about:blank' || gBrowser.webProgress.isLoadingDocument"+
+            ") ? 'tab' : 'current'; $&")); 
+    }catch(e){}
+
+	// 滚轮切换标签
+	gBrowser.mTabContainer.addEventListener("DOMMouseScroll", function(event){
+		this.advanceSelectedTab(event.detail > 0 ? +1 : -1, true);
+	}, true);
+
+	//中键点击bookmark菜单不关闭
     try {
         eval('BookmarksEventHandler.onClick =' + BookmarksEventHandler.onClick.toString().replace('node.hidePopup()', ''));
         eval('checkForMiddleClick =' + checkForMiddleClick.toString().replace('closeMenus(event.target);', ''));
     } catch(e) {}
-    //右键关闭标签页
+
+	//右键关闭标签页
     gBrowser.mTabContainer.addEventListener("click",
     function(e) {
         if (e.target.localName == "tab" && e.button == 2 && !e.ctrlKey) {
@@ -42,7 +51,8 @@
         }
     },
     false);
-    //鼠标停留标签自动聚焦
+
+	//鼠标停留标签自动聚焦
     (document.getElementById("tabbrowser-tabs") || gBrowser.mTabBox).addEventListener('mouseover',
     function self(e) {
         if ((self.target = e.target).localName === 'tab') {
@@ -60,5 +70,22 @@
         }
     },
     false);
+
+	// 标签上双击刷新
+	gBrowser.mTabContainer.addEventListener('dblclick', function (event){
+		if (event.target.localName == 'tab' && event.button == 0){
+			getBrowser().getBrowserForTab(event.target).reload();
+		}
+	}, false);
+	
+	//自动关闭下载产生的空白标签
+	eval("gBrowser.mTabProgressListener = " + gBrowser.mTabProgressListener.toString().replace(/(?=var location)/, '\
+      if (aWebProgress.DOMWindow.document.documentURI == "about:blank"\
+          && aRequest.QueryInterface(nsIChannel).URI.spec != "about:blank") {\
+        aWebProgress.DOMWindow.setTimeout(function() {\
+          !aWebProgress.isLoadingDocument && aWebProgress.DOMWindow.close();\
+        }, 100);\
+      }\
+    '));
 
 })();
