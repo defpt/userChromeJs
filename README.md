e16821aa59cce9d6d6d38195e999c22de0b130a9
defpt <defpt@outlook.com>
2014-02-26 10:18:18 +0100

    自用脚本备份

--- README.md
+++ README.md
@@ -1,34 +1,37 @@
-一些自用脚本的说明文档
-=======
+#自用脚本的说明文档
 
 **个人经常访问的脚本资源站点**：[紫云飞原创脚本](http://www.cnblogs.com/ziyunfei/archive/2011/11/25/2263756.html)、 [饭友ywzhaiqi自用脚本](https://github.com/ywzhaiqi/userChromeJS) 、 [饭友lastdream2013自用脚本](https://github.com/lastdream2013/userChrome)、 [Alice00775 原创资源](https://github.com/alice0775/userChrome.js)、 [Griever 原创资源](https://github.com/Griever/userChromeJS)、[Mozilla 社区脚本资源](https://j.mozest.com/zh-CN/)
 
 **下面是一些自用（包括自写、修改、整理的）UC脚本** [此处是一些按钮图样](https://github.com/defpt/userChromeJs/blob/master/Picture/README.md)
 
+##多文件脚本
+* [addMenuPlus](https://github.com/defpt/userChromeJs/tree/master/addMenuPlus) 神器级菜单设置脚本，我的必备脚本之一
 * [DefaultFullZoom](https://github.com/defpt/userChromeJs/tree/master/Default%20Full%20Zoom%20Level)  可设置默认缩放百分比
 * [KeyChanger](https://github.com/defpt/userChromeJs/tree/master/KeyChanger) 快捷键设置uc脚本，我的必备脚本之一
-* [MouseGestures](https://github.com/defpt/userChromeJs/tree/master/MouseGestures) 鼠标手势脚本
+* [MouseGestures](https://github.com/defpt/userChromeJs/tree/master/MouseGestures) 鼠标手势脚本，我的必备脚本之一
 * [RefererChange](https://github.com/defpt/userChromeJs/tree/master/RefererChange) 破解图片外链外置配置版
 * [Sidebar](https://github.com/defpt/userChromeJs/blob/master/Sidebar)   从lastdream2013版的SidebarMod.uc.js修改而来的侧边栏加强脚本
 * [Stylish](https://github.com/defpt/userChromeJs/tree/master/Stylish) stylish 扩展辅助编辑功能脚本
 * [Translator](https://github.com/defpt/userChromeJs/blob/master/Translator) 翻译脚本
-* [addMenuPlus](https://github.com/defpt/userChromeJs/tree/master/addMenuPlus) 神器，菜单设置脚本，我的必备脚本之一
-* [ContextMenu.uc.js](https://github.com/defpt/userChromeJs/blob/master/ContextMenu.uc.js) *右键菜单，添加了几个右键菜单，现在我已经弃用，比起addmenuplus脚本差多了*
-* [DownloadPlus.uc.js](https://github.com/defpt/userChromeJs/blob/master/DownloadPlus.uc.js)  右键点击下载按钮新建下载+下载重命名+另存为+自动关闭下载产生的空白标签+完成下载提示音，**以后不再维护这个脚本**
-* [EasyDrag.uc.js](https://github.com/defpt/userChromeJs/blob/master/EasyDrag.uc.js) 从紫大博客定制的自用拖拽脚本，修复一个拖拽链接bug以及添加一些自用功能
+
+##单文件脚本
+* [ContextMenu.uc.js](https://github.com/defpt/userChromeJs/blob/master/ContextMenu.uc.js) 右键菜单，添加了几个右键菜单，现在我已经弃用，比起addmenuplus脚本差多了
+* [contextSearcher.uc.js](https://github.com/defpt/userChromeJs/blob/master/contextSearcher.uc.js) 右键搜索增强脚本，基于原版添加了搜索完成后自动清空搜索栏并切回默认搜索引擎
+* [DownloadPlus.uc.js](https://github.com/defpt/userChromeJs/blob/master/DownloadPlus.uc.js)  右键点击下载按钮新建下载+下载重命名+另存为+自动关闭下载产生的空白标签+完成下载提示音，以后不再维护这个脚本
+* [EasyDrag.uc.js](https://github.com/defpt/userChromeJs/blob/master/EasyDrag.uc.js) 从紫大博客定制的自用拖拽脚本，修复一个拖拽链接bug以及添加一些自用功能，我的必备脚本之一
 * [ExternalAppBtn.uc.js](https://github.com/defpt/userChromeJs/blob/master/ExternalAppBtn.uc.js) 代替橙色菜单，以lastdream2013的代码为基础，整合了脚本 [MyMenu.uc.js](https://github.com/defpt/userChromeJs/blob/master/MyMenu.uc.js) 
 * [ExternalFuncBtn.uc.js](https://github.com/defpt/userChromeJs/blob/master/ExternalFuncBtn.uc.js)  多功能辅助按钮菜单， 以lastdream2013的代码为基础，整合了脚本 [JsToolsmenu.uc.js](https://github.com/defpt/userChromeJs/blob/master/JsToolsmenu.uc.js)
 * [HistoryMenu.uc.js](https://github.com/defpt/userChromeJs/blob/master/HistoryMenu.uc.js) 历史按钮、左键打开菜单，右击恢复最后关闭的标签 
-* [InFormEnterLite.uc.js](https://github.com/defpt/userChromeJs/blob/master/InFormEnterLite.uc.js) *填表脚本，被我搞成快速回复了，貌似这个比论坛灌水脚本支持范围更广，现已启用，理由同ContextMenu.uc.js*
+* [InFormEnterLite.uc.js](https://github.com/defpt/userChromeJs/blob/master/InFormEnterLite.uc.js) 填表脚本，被我搞成快速回复了，貌似这个比论坛灌水脚本支持范围更广，现已弃用，理由同ContextMenu.uc.js
 * [MemoryMonitor.uc.js](https://github.com/defpt/userChromeJs/blob/master/MemoryMonitor.uc.js) 简单的FF内存监视器
-* [ScrollWithMouse.uc.js](https://github.com/defpt/userChromeJs/blob/master/ScrollWithMouse.uc.js)   *免点击，光标放滚动条即可上下拖动页面滚动。用了一段时间，发现错误控制台错误挺多，现已弃用，改用油侯脚本了*
-* [TabPlus.uc.js](https://github.com/defpt/userChromeJs/blob/master/TabPlus.uc.js) 自用标签增强，整合多个功能
+* [ScrollWithMouse.uc.js](https://github.com/defpt/userChromeJs/blob/master/ScrollWithMouse.uc.js)   免点击，光标放滚动条即可上下拖动页面滚动。用了一段时间，发现错误控制台错误挺多，现已弃用，改用油侯脚本了
+* [TabPlus.uc.js](https://github.com/defpt/userChromeJs/blob/master/TabPlus.uc.js) 自用标签增强，整合多个功能，我的必备脚本之一
 * [Updatebookmarklite.uc.js](https://github.com/defpt/userChromeJs/blob/master/Updatebookmarklite.uc.js) 用来更新书签网址、名称
 * [UserCSSLoader.uc.js](https://github.com/defpt/userChromeJs/blob/master/UserCSSLoader.uc.js) 自用微改版样式管理
 * [UserScriptLoader.uc.js](https://github.com/defpt/userChromeJs/blob/master/UserScriptLoader.uc.js) 自用微改 uc 版油猴脚本加载器
-* [openImgRar.uc.js](https://github.com/defpt/userChromeJs/blob/master/openImgRar.uc.js) 解密图片链接
+* [openImgRar.uc.js](https://github.com/defpt/userChromeJs/blob/master/openImgRar.uc.js) 解密内涵图，已集成在addmenuplus脚本配置中
 * [quickProxyModokiMod.uc.js](https://github.com/defpt/userChromeJs/blob/master/quickProxyModokiMod.uc.js) 简单的快速代理切换
 * [rebuildAppmenu.uc.xul](https://github.com/defpt/userChromeJs/blob/master/rebuildAppmenu.uc.xul) 自用橙色菜单(FF20+)，**以后不再维护和更新**，FF29开始，火狐橙就要成为过去了
-* [rebuild_userChrome.uc.xul](https://github.com/defpt/userChromeJs/blob/master/rebuild_userChrome.uc.xul) 自用 UC 管理器，工具栏固定图标，精简了菜单
+* [rebuild_userChrome.uc.xul](https://github.com/defpt/userChromeJs/blob/master/rebuild_userChrome.uc.xul) 自用 UC 管理器，工具栏固定图标，精简了菜单，我的必备脚本之一
 * [speedStartFF.uc.js](https://github.com/defpt/userChromeJs/blob/master/speedStartFF.uc.js) 加速启动火狐
 * [Example](https://github.com/defpt/userChromeJs/blob/master/Example) 学习UC脚本笔记以及范例收藏
---
+++ contextSearcher.uc.js
@@ -0,0 +1,296 @@
+// ==UserScript==
+// @name           contextSearcher.uc.js
+// @namespace      http://d.hatena.ne.jp/Griever/
+// @description    右键搜索增强
+// @author         Griever
+// @include        main
+// @compatibility  Firefox 4
+// @version        基于原版 v0.0.8
+// @note           修改自用版：添加搜索完成后自动清空搜索栏并切回默认搜索引擎
+// ==/UserScript==
+
+if (window.contextSearcher) {
+  window.contextSearcher.destroy();
+  delete window.contextSearcher;
+}
+
+window.contextSearcher = {
+  NEW_TAB: Services.prefs.getBoolPref("browser.search.openintab"),
+
+  _regexp: {
+    hiragana: "[\\u3040-\\u309F]+",
+    katakana: "[\\u30A0-\\u30FA\\u30FC]+",
+    kanji   : "[\\u4E00-\\u9FA0]+",
+    suuji   : "[0-9_./,%-]+",
+    eisu_han: "\\w[\\w\\-]*",
+    eisu_zen: "[\\uFF41-\\uFF5A\\uFF21-\\uFF3A\\uFF10-\\uFF19]+",
+    hankaku : "[\\uFF00-\\uFFEF]+",
+    hangul  : "[\\u1100-\\u11FF\\uAC00-\\uD7AF\\u3130-\\u318F]+",
+  },
+
+  get startReg() {
+    let reg = {};
+    for(let n in this._regexp) {
+      reg[n] = new RegExp('^' + this._regexp[n]);
+    }
+    delete this.startReg;
+    return this.startReg = reg;
+  },
+  get endReg() {
+    let reg = {};
+    for(let n in this._regexp) {
+      reg[n] = new RegExp(this._regexp[n] + '$');
+    }
+    delete this.endReg;
+    return this.endReg = reg;
+  },
+  getCharType: function(aChar) {
+    var c = aChar.charCodeAt(0);
+    //if (c >= 0x30 && c <= 0x39) return "suuji";
+    if (c >= 0x30 && c <= 0x39 || c >= 0x41 && c <= 0x5A || c >= 0x61 && c <= 0x7A || c === 0x5F) return "eisu_han";
+    if (c >= 0x30A0 && c <= 0x30FA || c === 0x30FC) return "katakana";
+    if (c >= 0x3040 && c <= 0x309F) return "hiragana";
+    if (c >= 0x4E00 && c <= 0x9FA0) return "kanji";
+    if (c >= 0xFF41 && c <= 0x9F5A || c >= 0xFF21 && c <= 0xFF3A || c >= 0xFF10 && c <= 0xFF19) return "eisu_zen";
+    if (c >= 0xFF00 && c <= 0xFFEF) return "hankaku";
+    if (c >= 0x1100 && c <= 0x11FF || c >= 0xAC00 && c <= 0xD7AF || c >= 0x3130 && c <= 0x318F) return "hangul";
+    return "";
+  },
+
+  searchText: '',
+  searchEngines: [],
+  init: function(){
+    this.isMac = navigator.platform.indexOf("Mac") == 0;
+    this.searchService = Cc["@mozilla.org/browser/search-service;1"].getService(Ci.nsIBrowserSearchService);
+    this.context = document.getElementById('contentAreaContextMenu');
+    var searchselect = document.getElementById('mason-fcd-r1');
+	
+    this.menu = this.context.insertBefore(document.createElement('menu'), searchselect);
+    this.menu.setAttribute('id', 'context-searcher');
+    this.menu.setAttribute('class', 'menu-iconic');
+    //this.menu.setAttribute('accesskey', searchselect.accessKey);
+    this.menu.setAttribute('onclick', 'if (event.target == this) { contextSearcher.command(event); closeMenus(this); }');
+
+    this.popup = this.menu.appendChild( document.createElement('menupopup') );
+
+    this.context.addEventListener('popupshowing', this, false);
+    this.menu.addEventListener('DOMMouseScroll', this, false);
+    gBrowser.mPanelContainer.addEventListener(this.isMac ? 'mousedown' : 'click', this, false);
+    window.addEventListener('unload', this, false);
+  },
+
+  uninit: function() {
+    this.context.removeEventListener('popupshowing', this, false);
+    this.menu.removeEventListener('DOMMouseScroll', this, false);
+    gBrowser.mPanelContainer.removeEventListener('click', this, false);
+    gBrowser.mPanelContainer.removeEventListener('mousedown', this, false);
+    window.removeEventListener('unload', this, false);
+  },
+
+  destroy: function(){
+    this.uninit();
+    document.getElementById('context-searchselect').style.removeProperty('display');
+    var m = document.getElementById('context-searcher');
+    if (m)
+      m.parentNode.removeChild(m);
+  },
+
+  handleEvent: function(event) {
+    if (this[event.type])
+      this[event.type](event);
+  },
+  
+  unload: function(e){
+    this.uninit();
+  },
+
+  DOMMouseScroll: function(e) {
+    if (this.searchEngines.length === 0)
+      this.searchEngines = this.searchService.getVisibleEngines({});
+    if (!this.searchEngines || this.searchEngines.length == 0)
+      return;
+    
+    var index = this.searchEngines.indexOf(this.searchService.currentEngine);
+    var newEngine = e.detail > 0? this.searchEngines[index+1] : this.searchEngines[index-1];
+    if (!newEngine)
+      return;
+    this.searchService.currentEngine = newEngine;
+    this.setMenuitem();
+  },
+
+  command: function(e){
+    var target = e.target;
+    var engine = e.target.engine || this.menu.engine;
+
+    var submission = engine.getSubmission(this.searchText, null);
+    if (!submission)
+      return;
+
+    var where = whereToOpenLink(e);
+    if (this.NEW_TAB && where === 'current' || where === 'save')
+      where = 'tab';
+
+    openLinkIn(submission.uri.spec, where, {
+      postData: submission.postData,
+      relatedToCurrent: true
+    });
+  },
+
+  click: function(event) {
+    if (event.button === 2) {
+      this._clickNode = event.rangeParent;
+      this._clickOffset = event.rangeOffset;
+      this._clientX = event.clientX;
+    } else {
+      this._clickNode = null;
+      this._clickOffset = 0;
+      this._clientX = 0;
+    }
+  },
+
+  mousedown: function(event) {
+    this.click(event);
+  },
+
+  setMenuitem: function() {
+    var currentEngine = this.searchService.currentEngine;
+    var l = this.searchText.length > 6? this.searchText.substr(0, 6) + '...' : this.searchText;
+    this.menu.engine = currentEngine;
+    //this.menu.setAttribute('label', gNavigatorBundle.getFormattedString("contextMenuSearchText", [currentEngine.name, l]));
+    this.menu.setAttribute('label', '搜索 "' + l + '"');
+    //this.menu.setAttribute('tooltiptext', currentEngine.name);
+    if (currentEngine.iconURI)
+      this.menu.style.listStyleImage = 'url("' + currentEngine.iconURI.spec + '")';
+    else 
+      this.menu.style.removeProperty('list-style-image');
+  },
+
+  popupshowing: function(e){
+    if (e.target != this.context) return;
+
+    this.searchText = 
+      gContextMenu.onTextInput? this.getTextInputSelection() :
+      gContextMenu.isTextSelected? this.getBrowserSelection() :
+      gContextMenu.onImage? gContextMenu.target.getAttribute('alt') :
+      gContextMenu.onLink? gContextMenu.linkText() :
+      this.getCursorPositionText();
+
+    if (!this.searchText || !/\S/.test(this.searchText)) {
+      this.menu.hidden = true;
+      return;
+    }
+    if (this.searchText.length > 256)
+      this.searchText = this.searchText.substr(0, 256);
+    this.menu.hidden = false;
+    
+    if (!this.popup.hasChildNodes() || e.ctrlKey)
+      this.createMenuitem();
+    
+    this.setMenuitem();
+  },
+  
+  createMenuitem: function(){
+    this.searchEngines = this.searchService.getVisibleEngines({});
+    if (!this.searchEngines || this.searchEngines.length == 0)
+      return;
+
+    var f;
+    while (f = this.popup.firstChild) {
+      this.popup.removeChild(f);
+    }
+
+    this.menu.engine = this.searchService.currentEngine;
+    if (this.menu.engine.iconURI)
+      this.menu.style.listStyleImage = 'url("' + this.menu.engine.iconURI.spec + '")';
+    else 
+      this.menu.style.removeProperty('list-style-image');
+    for (var i = 0, s = this.searchEngines, l = s.length; i < l; i++) {
+      var engine = s[i];
+      var m = document.createElement('menuitem');
+      m.setAttribute('label', engine.name);
+      m.setAttribute('class', 'menuitem-iconic bookmark-item');
+      if (engine.iconURI) {
+        m.setAttribute('image', engine.iconURI.spec);
+      }
+      m.setAttribute('oncommand', 'contextSearcher.command(event);');
+      m.setAttribute('onclick', 'checkForMiddleClick(this, event);');
+      m.engine = engine;
+      this.popup.appendChild(m);
+    }
+  },
+  
+  getBrowserSelection: function () {
+    var win = document.commandDispatcher.focusedWindow;
+    var sel = win.getSelection();
+    var str = '';
+    if (sel.isCollapsed)
+      return str;
+
+    for(var i = 0, l = sel.rangeCount; i < l; i++) {
+      str += sel.getRangeAt(i) + ' ';
+    }
+    return str.replace(/^\s*|\s*$/g, '').replace(/\s+/g, ' ');
+  },
+  
+  getTextInputSelection: function () {
+    var elem = document.commandDispatcher.focusedElement;
+    var str = elem.value.slice(elem.selectionStart, elem.selectionEnd);
+    return str.replace(/^\s*|\s*$/g, '').replace(/\s+/g, ' ');
+  },
+
+  getCursorPositionText: function() {
+    var node = this._clickNode;
+    var offset = this._clickOffset;
+    if (!node || node.nodeType !== Node.TEXT_NODE)
+      return "";
+
+    var text = node.nodeValue;
+
+    // 文字の右半分をクリック時に次の文字を取得する対策
+    var range = node.ownerDocument.createRange();
+    range.setStart(node, offset);
+    var rect = range.getBoundingClientRect();
+    range.detach();
+    if (rect.left >= this._clientX)
+      offset--;
+
+    if (!text[offset]) return "";
+    var type = this.getCharType(text[offset]);
+    if (!type) return "";
+
+    var mae = text.substr(0, offset);
+    var ato = text.substr(offset); // text[offset] はこっちに含まれる
+    var ato_word = (this.startReg[type].exec(ato) || [""])[0];
+    var str = this.endReg[type].test(mae) ? RegExp.lastMatch + ato_word : ato_word;
+
+    if (str.length === 1) {
+      if (type === "kanji") {
+        if (this.startReg["hiragana"].test(ato.substr(ato_word.length)))
+          str += RegExp.lastMatch;
+      } else {
+        return "";
+      }
+    }
+    
+    return str;
+  },
+  
+  log: function() {
+    Application.console.log("[contextSearcher] " + Array.slice(arguments));
+  }
+}
+
+window.contextSearcher.init();
+
+//搜索完成后自动清空搜索栏并切回默认搜索引擎
+(function() {
+	var searchbar = document.getElementById("searchbar");
+	searchbar._doSearchInternal = searchbar.doSearch;
+	searchbar.doSearch = function(aData, aInNewTab) {
+		this._doSearchInternal(aData, aInNewTab);
+		// 清空搜索栏
+		this.value = "";
+		// 切回默认引擎
+		this.currentEngine = this.engines ? this.engines[0] : this._engines[1];
+	};
+}()); 
