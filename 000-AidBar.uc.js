a42891ccc3ed2a96c894447006193a095a8dc39d
defpt <defpt@outlook.com>
2014-05-14 11:20:23 +0200

    修复启动时候显示1s问题

--- 000-AidBar.uc.js
+++ 000-AidBar.uc.js
@@ -13,19 +13,16 @@
 		aidBar.setAttribute('id', 'TabsToolbar_aidBar');
 		aidBar.setAttribute("label", "辅助栏");
 		aidBar.setAttribute("class", "toolbar");
+		aidBar.setAttribute("style", "overflow:hidden; max-width:16px; opacity:0; transition: 0.8s; transition-delay:1s;");
 		TabsToolbar.insertBefore(aidBar, document.getElementById("alltabs-button"));
 		document.insertBefore(document.createProcessingInstruction('xml-stylesheet', 'type="text/css" href="data:text/css;utf-8,' + encodeURIComponent('\
-#TabsToolbar_aidBar{\
-    overflow:hidden;\
-    max-width:16px;/*设置触发区宽度*/\
-    opacity:0;\
-    transition: 0.8s;\
-	transition-delay:2s;\
-}\
 #TabsToolbar_aidBar:hover{\
    max-width:300px;/*300px足够用了吧？*/\
     opacity:1;\
 	transition-delay:.2s;\
+}\
+#TabsToolbar_aidBar > image, .statusbarpanel-iconic{\
+	padding:5px 2px;\
 }\
 ') + '"'), document.documentElement);
 	}
