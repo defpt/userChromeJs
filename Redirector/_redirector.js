762073c1bb779c1e94642e9fec900fef93fc9597
defpt <defpt@outlook.com>
2014-05-30 10:45:05 +0200

    同步一下，没有什么更新

--- Redirector/_redirector.js
+++ Redirector/_redirector.js
@@ -6,32 +6,31 @@ rules = [
 		wildcard: false,                        // 可选，true 表示 from 是通配符
 		regex: false,                           // 可选，true 表示 from 是正则表达式
 		resp: false                             // 可选，true 表示替换 response body
-	},/* {
-		name: "google链接加密",
-		from: /^http:\/\/(([^\.]+\.)?google\..+)/i,
-		exclude: /google\.cn/i,                 // 可选，排除例外规则
-		to: "https://$1",
-		regex: true
-	}, */{
-		name: "google搜索禁止跳转",
-		from: /^https?:\/\/www\.google\.com\/url\?.*url=([^&]*).*/i,
-		to: "$1",
-		regex: true
-	},{
+	},
+	{
 		name: "wiki中文>>简体中文",
 		from: /\:\/\/zh\.wikipedia\.org\/(?!zh\-cn\/)[^\/]+/i,
 		exclude: /favicon\.ico/i,  
 		to: '://zh.wikipedia.org/zh-cn',
 		regex: true
-	},{
+	},
+	{
 		name: "userscripts:8080",
 		from: /^https?:\/\/userscripts\.org\/(.*)/i,
 		to: "http:\/\/userscripts.org:8080/$1",
 		regex: true
-	},{
+	},
+	{
 		name: "WiKi链接加密",
 		from: /^http:\/\/([^\/]+\.wikipedia\.org\/.+)/i,
 		to: "https://$1",
 		regex: true
+	},
+	{
+	    // 包含手机版界面
+	    name: "百度随心听（音质改320）",
+	    from: /^http:\/\/music\.baidu\.com\/data\/music\/fmlink(.*[&\?])rate=[^3]\d+(.*)/i,
+	    to: "http://music.baidu.com/data/music/fmlink$1rate=320$2",
+	    regex: true
 	}
 ];
